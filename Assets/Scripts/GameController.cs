79623d8ad259ef4b922b91a5128622e2e2fa2c84
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-27 13:08:05 +0300

    create tiles

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
+    public GameObject TilePrefab;
+
+    public GameObject Grid;
 
 	// Use this for initialization
 	void Start () {
-		
+	   
 	}
 	
 	// Update is called once per frame
