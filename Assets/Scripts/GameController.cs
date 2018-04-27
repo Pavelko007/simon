bb6a51868734f99c1a1e52993b07289e004e3eeb
Pavlo Kvas <kvaspasha@gmail.com>
2018-04-27 17:33:50 +0300

    play a sequence

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -11,27 +12,61 @@ public class GameController : MonoBehaviour
     private List<Tile> tiles = new List<Tile>();
 
 	// Use this for initialization
-	void Start ()
+	void Awake ()
 	{
-	    int tileIndx = 0;
-	    foreach (var tile in Grid.GetComponentsInChildren<Tile>())
-	    {
-            tiles.Add(tile);
-	        EventTrigger.Entry entry = new EventTrigger.Entry {eventID = EventTriggerType.PointerClick};
-	        var indx = tileIndx;
-	        entry.callback.AddListener(x=>{ TileClicked(indx);});
-	        var eventTrigger = tile.gameObject.AddComponent<EventTrigger>();
-	        eventTrigger.triggers.Add(entry);
-	        tileIndx++;
-	    }
+	    InitTiles();
 	}
+
+    void Start()
+    {
+        int numSteps = 3;
+        Queue<int> indxSequence = new Queue<int>();
+        for (int i = 0; i < numSteps; i++)
+        {
+            indxSequence.Enqueue(Random.Range(0, tiles.Count));
+        }
+
+        StartCoroutine(PlaySequence(indxSequence));
+    }
+
+    IEnumerator PlaySequence(Queue<int> indxSequence)
+    {
+        while (indxSequence.Count > 0)
+        {
+            int curTileIndx = indxSequence.Dequeue();
+            var tile = tiles[curTileIndx];
+
+            HighlightTile(tile);
+            var animator = tile.GetComponent<Animator>();
+            var animationTime = animator.GetCurrentAnimatorStateInfo(0).length ;
+
+            Debug.Log(animationTime);
+            yield return new WaitForSeconds(animationTime);
+        }
+    }
+    private void InitTiles()
+    {
+        int tileIndx = 0;
+        foreach (var tile in Grid.GetComponentsInChildren<Tile>())
+        {
+            tiles.Add(tile);
+            EventTrigger.Entry entry = new EventTrigger.Entry {eventID = EventTriggerType.PointerClick};
+            var indx = tileIndx;
+            entry.callback.AddListener(x => { TileClicked(indx); });
+            var eventTrigger = tile.gameObject.AddComponent<EventTrigger>();
+            eventTrigger.triggers.Add(entry);
+            tileIndx++;
+        }
+    }
 
     private void TileClicked(int tileIndx)
     {
-        tiles[tileIndx].GetComponent<Animator>().SetTrigger("highlight");
-
-        Debug.Log(string.Format("tile {0} was clicked", tileIndx));
+        var tile = tiles[tileIndx];
+        HighlightTile(tile);
     }
 
-    
+    private static void HighlightTile(Tile tile)
+    {
+        tile.GetComponent<Animator>().SetTrigger("highlight");
+    }
 }
